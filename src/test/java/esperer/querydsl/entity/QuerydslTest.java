0fc630f8ebffe18151e80775b4358d6a28157d2f
aesperer <piyrw9754@gmail.com>
2023-06-26 09:57:39 +0900

    projection simple

--- src/test/java/esperer/querydsl/entity/QuerydslTest.java
+++ src/test/java/esperer/querydsl/entity/QuerydslTest.java
@@ -432,4 +432,31 @@ class QuerydslTest {
                 .fetchOne();
     }
 
+    @Test
+    public void simpleProjection() {
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void simpleTupleProjection(){
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age)
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            System.out.println("username = " + username);
+            System.out.println("age = " + age);
+        }
+    }
+
 }
