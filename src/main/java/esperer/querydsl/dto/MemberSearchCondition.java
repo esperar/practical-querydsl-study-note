4059362310ffa4d40c76379d3fff3b06ea323038
aesperer <piyrw9754@gmail.com>
2023-06-29 12:21:36 +0900

    member dto

---
+++ src/main/java/esperer/querydsl/dto/MemberSearchCondition.java
@@ -0,0 +1,12 @@
+package esperer.querydsl.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberSearchCondition {
+
+    private String username;
+    private String teamName;
+    private Integer ageGoe;
+    private Integer ageLoe;
+}
