fa7fd79707f4b33d2d0c51c75c986f3b0dc8454f
aesperer <piyrw9754@gmail.com>
2023-06-30 14:42:28 +0900

    search member controller

--- src/main/java/esperer/querydsl/MemberController.java
+++ src/main/java/esperer/querydsl/MemberController.java
@@ -3,7 +3,10 @@ package esperer.querydsl;
 import esperer.querydsl.dto.MemberSearchCondition;
 import esperer.querydsl.dto.MemberTeamDto;
 import esperer.querydsl.repository.MemberJpaRepository;
+import esperer.querydsl.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +17,21 @@ import java.util.List;
 public class MemberController {
 
     private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
 
-    @GetMapping("/v1/member")
+    @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition){
         return memberJpaRepository.search(condition);
     }
+
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageSimple(condition, pageable);
+    }
+
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageComplex(condition, pageable);
+    }
+
 }
